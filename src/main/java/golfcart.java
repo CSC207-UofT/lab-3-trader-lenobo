9407bba6a90ec2ec1908297419bfd07cd10f8b18
nathan hansen <nathan.hansen@mail.utoronto.ca>
2021-09-27 19:25:17 -0400

    golf part 2

--- src/main/java/golfcart.java
+++ src/main/java/golfcart.java
@@ -1,7 +1,1 @@
-public class golfcart implements Drivable, Tradable{ private int maxSpeed;public golfcart() {this.maxSpeed = 9999;}public void upgradeSpeed() {this.maxSpeed++;}public void downgradeSpeed() {
-        this.maxSpeed--;
-    }public int getMaxSpeed() {
-        return this.maxSpeed;
-    }@Override public int getPrice() {
-        return 9999;
-    }}
+public class golfcart implements Drivable, Tradable{ private int maxSpeed;public golfcart() {this.maxSpeed = 9999;}public void upgradeSpeed() {this.maxSpeed++;}public void downgradeSpeed() {this.maxSpeed--;}public int getMaxSpeed() {return this.maxSpeed;public int getPrice() {return 9999;}}

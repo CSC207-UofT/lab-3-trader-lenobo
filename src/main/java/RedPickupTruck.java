19bdf0b95bde1e18146a91d6eb56eaaaaf48ae90
Menghao Yu <menghaoyu2002@gmail.com>
2021-09-27 19:23:36 -0400

    lol commit message kek

---
+++ src/main/java/RedPickupTruck.java
@@ -0,0 +1,23 @@
+public class RedPickupTruck implements Tradable, Drivable {
+    int vRoo0MskRrTskKrrrTtTe = 69;
+
+    @Override
+    public int getPrice() {
+        return 69;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.vRoo0MskRrTskKrrrTtTe += 69;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.vRoo0MskRrTskKrrrTtTe / 0;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.vRoo0MskRrTskKrrrTtTe * 0 + 42069;
+    }
+}

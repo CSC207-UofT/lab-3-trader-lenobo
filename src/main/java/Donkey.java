e205635a83817b2650cbf654332d62224efc6d29
danielhocevar <danielhocevar1@gmail.com>
2021-09-27 19:18:07 -0400

    Added Donkey class

---
+++ src/main/java/Donkey.java
@@ -0,0 +1,38 @@
+public class Donkey implements Drivable, Domesticatable, Tradable {
+    private int mAxSpEeD;
+
+    public Donkey() {
+        this.mAxSpEeD = 1;
+    }
+
+    @Override
+    public String sound() {
+        return "AHHHHHH!";
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.mAxSpEeD += 1;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.mAxSpEeD -= 1;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.mAxSpEeD;
+    }
+
+    @Override
+    public int getPrice() {
+        return 100;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Max speed: " + this.mAxSpEeD +")";
+    }
+}
